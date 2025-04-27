efe499991a5291a48159678fe59296958aea1c03
Enrique Plata <>
2025-04-27 13:27:04 +0200

    Adding integration code

--- infrastructure/aws/outputs.tf
+++ infrastructure/aws/outputs.tf
@@ -4,3 +4,7 @@ output "s3_role_arn" {
   description = "The ARN of the s3_role"
   value       = aws_iam_role.s3_role.arn
 }
+output "s3_bucket_name" {
+  description = "The name of the s3 bucket"
+  value       = aws_s3_bucket.s3_bucket.bucket
+}
